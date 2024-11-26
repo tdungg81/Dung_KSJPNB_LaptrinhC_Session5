9f5cde80daf4fa19b9a13c858fe9671e89d2efeb
tdungg81 <dungnguyen8120@gmail.com>
2024-11-26 10:18:23 +0700

    Ex5_Session5

---
+++ Ex5_Session5.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  
+    for (int a = 1; a <= 9; a++) {
+        printf("Bang cuu chuong cua %d:\n", a);
+        for (int i = 1; i <= 10; i++) {
+            printf("%d x %d = %d\n", a, i, a * i);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
